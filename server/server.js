3b3f7bb6959923b3b72e690e5dffad77e6febf2f
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2019-08-05 17:41:52 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ var express = require('express');
 
 var publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
-
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Server is up on port', port);
 })
 
